01f6889003156c43c8a8a81032fce1db5848dbc3
MdioufDataScientist <mohameddiouf2@ep.sn>
2021-12-25 21:41:09 +0100

    mis en place des fonctions et des callbacks

--- page_1.py
+++ page_1.py
@@ -1,7 +1,7 @@
 from dash import dcc
 from dash import html
 import dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from dash.html.Center import Center
 import plotly.express as px
 import plotly.graph_objects as go
@@ -13,7 +13,7 @@ from datetime import date
 #path="https://github.com/MdioufDataScientist/Projet-Visualisation/blob/main/COVID-19-geographic-disbtribution-worldwide%20-%20COVID-19-geographic-disbtributi.csv"
 path1="/home/mdiouf/Bureau/Projet-Visualisation/Data_covid.csv"
 data=pd.read_csv(path1)
-print(data.head(5))
+#print(data.head(5))
 
 load_figure_template("cyborg")
 app=dash.Dash(__name__,external_stylesheets=[dbc.themes.CYBORG])
@@ -24,21 +24,31 @@ app.layout=dbc.Container([
     dbc.Row([       
             dbc.Col([
                 dcc.DatePickerRange(
-            display_format="DD/M/YYYY",
+            id="plage_de_date",
+            #display_format="DD/M/YYYY",
             min_date_allowed=date(2019, 12, 31),
             max_date_allowed=date(2020, 3, 27),
             start_date_placeholder_text="Debut",
             end_date_placeholder_text="Fin",
             calendar_orientation='vertical',
             )
-            ], width={'size':6}
+            ], width={'size':4}
             ),
             dbc.Col([
             dcc.Dropdown(id='d_drp',
             options=[{'label':x,'value':x} for x in data['countriesAndTerritories'].unique()],
             placeholder="selectionner le Pays"
             )
-            ], width={'size':6}) 
+            ], width={'size':4}),
+            dbc.Col([
+                dbc.Button(
+            id="submit_button",
+            n_clicks=0,
+            children="Submit",
+            color="primary",
+            #block=True,
+             ),
+            ], width={'size':4})
         ]
         ),
         dbc.Row([
@@ -102,5 +112,31 @@ app.layout=dbc.Container([
         )       
     ]),
 ])
+data['dateRep']=pd.to_datetime(data['dateRep'])
+def creer_datafram(debut,fin,pays):
+    df=data[data['countriesAndTerritories']==pays]
+    mask = (df['dateRep'] >= debut) & (df['dateRep'] <= fin)
+    df_filtrer=df.loc[mask]
+    df_filtrer.sort_values(by="dateRep",ascending=True)
+    print(type(debut))
+    #print(df_filtrer)
+    return df_filtrer
+
+@app.callback(
+Output('fig_1', 'figure'),
+[Input('plage_de_date', 'start_date'),
+Input('plage_de_date', 'end_date'),
+Input('d_drp','value')
+])
+def update(debut,fin,pays):
+    if pays:
+        if debut:
+            if fin:
+                df=creer_datafram(debut,fin,pays)
+                fig1=px.pie(df,values='cases',names='dateRep',color_discrete_sequence=px.colors.sequential.RdBu,title=f"Nombre de cas pa Jour : {pays}")
+                return fig1
+    else:
+        fig1={}  
+        return fig1
 
 app.run_server(debug=True)
