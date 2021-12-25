65f7d0f2928eaeb0a03b563b49763402f845a1d5
MdioufDataScientist <mohameddiouf2@ep.sn>
2021-12-25 16:30:52 +0100

    mis à jours

--- page_1.py
+++ page_1.py
@@ -8,9 +8,12 @@ import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
 from dash_bootstrap_templates import load_figure_template
 import pandas as pd
+from datetime import date
 
-path=""
-
+#path="https://github.com/MdioufDataScientist/Projet-Visualisation/blob/main/COVID-19-geographic-disbtribution-worldwide%20-%20COVID-19-geographic-disbtributi.csv"
+path1="/home/mdiouf/Bureau/Projet-Visualisation/Data_covid.csv"
+data=pd.read_csv(path1)
+print(data.head(5))
 
 load_figure_template("cyborg")
 app=dash.Dash(__name__,external_stylesheets=[dbc.themes.CYBORG])
@@ -21,15 +24,18 @@ app.layout=dbc.Container([
     dbc.Row([       
             dbc.Col([
                 dcc.DatePickerRange(
-            start_date_placeholder_text="Start Period",
-            end_date_placeholder_text="End Period",
+            display_format="DD/M/YYYY",
+            min_date_allowed=date(2019, 12, 31),
+            max_date_allowed=date(2020, 3, 27),
+            start_date_placeholder_text="Debut",
+            end_date_placeholder_text="Fin",
             calendar_orientation='vertical',
             )
             ], width={'size':6}
             ),
             dbc.Col([
             dcc.Dropdown(id='d_drp',
-            #options=[{'label':x,'value':x} for x in data['Year'].sort_values(ascending=False).unique()],
+            options=[{'label':x,'value':x} for x in data['countriesAndTerritories'].unique()],
             placeholder="selectionner le Pays"
             )
             ], width={'size':6}) 
